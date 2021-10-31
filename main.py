2d2662eb1d4cd69c516b9408ecf44a1b909f9d2e
T_compute2018 <72470140+Tcompute2018@users.noreply.github.com>
2021-10-30 20:45:16 -0500

    updated

--- main.py
+++ main.py
@@ -2,13 +2,34 @@ import pygame
 # import time
 from pygame.locals import * # import KEYDOWN pygame.event
 
+
+def draw_block():
+    surface.fill((92,25,84)) # add background to white color  // call this function, will update the screen to remove all blocks 
+    surface.blit(block,(block_x,block_y)) # draw block
+    pygame.display.flip() # this function is to update the function above otherwise it wont work can also use with .update
+
+
+
 if __name__ == "__main__":
     pygame.init()
 
     surface = pygame.display.set_mode((1000,500)) #window size  # initilize a window or screen for display
 
     surface.fill((92,25,84)) # add background to white color  
+
+
+
+    #  # load the image
+    block = pygame.image.load("blueB.png").convert()
+    block_x = 100 # give the block initial location in the screen
+    block_y = 100
+    surface.blit(block,(block_x,block_y)) # draw block
+
+
+
     pygame.display.flip() # this function is to update the function above
+
+    
 
     # time.sleep(5) #window appear for 5 seconds
     # instead of time let stop the game when click on the escape.
@@ -16,6 +37,27 @@ if __name__ == "__main__":
     while running:
         for event in pygame.event.get():
             if event.type == KEYDOWN:
-                pass
+                if event.key == K_ESCAPE:  # this is for when you press escape key, this will close the window
+                    running = False 
+                if event.key == K_UP:
+                    block_y -= 10
+                    draw_block()
+                if event.key == K_DOWN:
+                    block_y += 10
+                    draw_block()
+                if event.key == K_LEFT:
+                    block_x -= 10
+                    draw_block()
+                if event.key == K_RIGHT:
+                    block_x += 10
+                    draw_block()
+
             elif event.type == QUIT: # quit when click on X
                 running = False
+
+    # draw blocks
+
+
+
+
+
