6e5611649a4a0d58c0427f3cea0a4d338b465b2c
T_compute2018 <72470140+Tcompute2018@users.noreply.github.com>
2021-11-01 14:23:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,28 @@ import pygame
 # import time
 from pygame.locals import * # import KEYDOWN pygame.event
 
+class Snake:
+    def __init__(self, parent_screen):
+         #  # load the image
+        self.parent_screen = parent_screen
+        self.block = pygame.image.load("blueB.png").convert()
+        self.x = 100 # give the block initial location in the screen
+        self.y = 100
+
+    def draw(self):
+        self.parent_screen.blit(self.block,(self.x,self.y)) # draw block
+
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.surface = pygame.display.set_mode((1000,500)) #window size  # initilize a window or screen for display
+        self.surface.fill((92,25,84)) # add background to white color  
+        self.snake = Snake(self.surface) # window
+        self.snake.draw() # draw the snake
+    def run(self):
+        pass
 
 def draw_block():
     surface.fill((92,25,84)) # add background to white color  // call this function, will update the screen to remove all blocks 
@@ -11,25 +33,15 @@ def draw_block():
 
 
 if __name__ == "__main__":
-    pygame.init()
 
-    surface = pygame.display.set_mode((1000,500)) #window size  # initilize a window or screen for display
-
-    surface.fill((92,25,84)) # add background to white color  
+    game = Game()
+    game.run()
 
 
 
-    #  # load the image
-    block = pygame.image.load("blueB.png").convert()
-    block_x = 100 # give the block initial location in the screen
-    block_y = 100
     surface.blit(block,(block_x,block_y)) # draw block
 
-
-
     pygame.display.flip() # this function is to update the function above
-
-    
 
     # time.sleep(5) #window appear for 5 seconds
     # instead of time let stop the game when click on the escape.
