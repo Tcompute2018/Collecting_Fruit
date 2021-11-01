1a80ff6fe2e3a7c82a3ab068b9ecd29293b4c169
T_compute2018 <72470140+Tcompute2018@users.noreply.github.com>
2021-11-01 14:29:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,22 @@ class Snake:
         self.y = 100
 
     def draw(self):
+        self.surface.fill((92,25,84)) # add background to white color  // call this function, will update the screen to remove all blocks 
         self.parent_screen.blit(self.block,(self.x,self.y)) # draw block
+        pygame.display.flip() # this function is to update the function above otherwise it wont work can also use with .update
 
-
+    def move_left(self):
+        self.x -= 10
+        self.draw()
+    def move_right(self):
+        self.x += 10
+        self.draw()
+    def move_up(self):
+        self.y -= 10
+        self.draw()
+    def move_down(self):
+        self.y += 10
+        self.draw()
 
 class Game:
     def __init__(self):
@@ -22,14 +35,35 @@ class Game:
         self.surface.fill((92,25,84)) # add background to white color  
         self.snake = Snake(self.surface) # window
         self.snake.draw() # draw the snake
+
     def run(self):
-        pass
+            # time.sleep(5) #window appear for 5 seconds
+              # instead of time let stop the game when click on the escape.
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:  # this is for when you press escape key, this will close the window
+                        running = False 
+                    if event.key == K_UP:
+                        self.snake.move_UP()
+                        block_y -= 10
+                        draw_block()
+                    if event.key == K_DOWN:
+                        self.snake.move_DOWN()
+                        block_y += 10
+                        draw_block()
+                    if event.key == K_LEFT:
+                        self.snake.move_left()
+                     
+                    if event.key == K_RIGHT:
+                        self.snake.move_right()
+                        draw_block()
 
-def draw_block():
-    surface.fill((92,25,84)) # add background to white color  // call this function, will update the screen to remove all blocks 
-    surface.blit(block,(block_x,block_y)) # draw block
-    pygame.display.flip() # this function is to update the function above otherwise it wont work can also use with .update
+                elif event.type == QUIT: # quit when click on X
+                    running = False
 
+    # draw blocks
 
 
 if __name__ == "__main__":
@@ -39,9 +73,6 @@ if __name__ == "__main__":
 
 
 
-    surface.blit(block,(block_x,block_y)) # draw block
-
-    pygame.display.flip() # this function is to update the function above
 
     # time.sleep(5) #window appear for 5 seconds
     # instead of time let stop the game when click on the escape.
